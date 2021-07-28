d861660d89bf5e1383d311ec8427cecb7fce39b6
Alexandra <alexadubinina@mail.ru>
2021-07-28 10:44:38 +0300

    Generated Project

---
+++ core/src/ru/geekbrains/StarGame.java
@@ -0,0 +1,36 @@
+package ru.geekbrains;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion region;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		region= new TextureRegion(img, 25, 25, 100, 60);
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.draw(img, 300, 300, 100, 100);
+		batch.draw(region, 200, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
